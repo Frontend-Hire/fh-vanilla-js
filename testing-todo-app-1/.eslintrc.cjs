1aadb70616bdadca4a7bbe5785a16cd231aff78c
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-07-30 03:30:17 +0530

    test init

--- testing-todo-app-1/.eslintrc.cjs
+++ testing-todo-app-1/.eslintrc.cjs
@@ -6,6 +6,8 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:testing-library/react',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
