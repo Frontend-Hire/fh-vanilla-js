0edbfd5ffcf71fe1565ae39c4ea5437a7c5295c4
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-08-05 15:10:11 +0530

    question final

--- ts-migrate-use-state-1/src/App.jsx
+++ ts-migrate-use-state-1/src/App.jsx
@@ -3,15 +3,40 @@ import AddTask from './AddTask';
 import Task from './Task';
 
 export default function App() {
-  const tasks = [{ id: 1, taskName: 'First Task' }];
+  const [tasks, setTasks] = React.useState([{ id: 1, taskName: 'First Task' }]);
 
-  const addTask = () => {};
+  const addTask = (task) => {
+    const trimmedTask = task.trim();
 
-  const deleteTask = () => {};
+    if (!trimmedTask) {
+      return;
+    }
+
+    const newTask = {
+      id: Math.random().toString(36).substr(2, 9),
+      taskName: trimmedTask,
+    };
+
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const clearAllTasks = () => {
+    setTasks([]);
+  };
 
   return (
     <div className="px-4 py-2">
       <AddTask onAdd={addTask} />
+      <button
+        onClick={clearAllTasks}
+        className="border border-red-500 bg-red-500 p-2 text-white"
+      >
+        Clear All
+      </button>
       {tasks.map((task, idx) => (
         <Task
           key={task.id}
