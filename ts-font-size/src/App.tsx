5ba7b2934af88f82d55d426581a0794a33ade05f
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-08-20 00:06:54 +0530

    final update

--- ts-font-size/src/App.tsx
+++ ts-font-size/src/App.tsx
@@ -5,6 +5,8 @@ export default function App() {
     <div className="space-y-4 px-4 py-2">
       <Text>This should be a small text!</Text>
 
+      <Text>This should be the default size (16px) text!</Text>
+
       <Text>This should be a large text!</Text>
 
       <Text>This should have a size of 50px</Text>
