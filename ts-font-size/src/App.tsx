31e63020c2bd0523fa0783d6c797e17de16fd0da
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-08-19 23:54:50 +0530

    feat: question final

--- ts-font-size/src/App.tsx
+++ ts-font-size/src/App.tsx
@@ -1,3 +1,15 @@
+import Text from './Text';
+
 export default function App() {
-  return <div className="px-4 py-2">Hello World!</div>;
+  return (
+    <div className="space-y-4 px-4 py-2">
+      <Text>This should be a small text!</Text>
+
+      <Text>This should be a large text!</Text>
+
+      <Text>This should have a size of 50px</Text>
+
+      <Text>This should have a size of 5rem</Text>
+    </div>
+  );
 }
---
+++ ts-font-size/src/Text.tsx
@@ -0,0 +1,3 @@
+export default function Text({ children }: React.PropsWithChildren) {
+  return <p>{children}</p>;
+}
